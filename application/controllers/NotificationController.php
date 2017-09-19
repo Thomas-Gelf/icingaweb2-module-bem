d0dd574cb47fa82ca97073bd59412697f0112226
Thomas Gelf <thomas@gelf.net>
2017-09-19 15:00:14 +0200

    NotificationController: show more details

--- application/controllers/NotificationController.php
+++ application/controllers/NotificationController.php
@@ -3,36 +3,61 @@
 namespace Icinga\Module\Bem\Controllers;
 
 use ipl\Html\Html;
+use ipl\Web\Widget\NameValueTable;
 
 class NotificationController extends ControllerBase
 {
     public function indexAction()
     {
         if ($service = $this->params->get('service')) {
-            $checksum = sha1(
-                $this->params->get('host')
-                . $this->params->get('service'),
-                true
-            );
+            $host = $this->params->get('host');
+            $checksum = sha1($host . $service, true);
+            $this->addTitle('%s: %s', $host, $service);
         } else {
-            $checksum = sha1(
-                $this->params->get('host'),
-                true
-            );
+            $host = $this->params->get('host');
+            $service = null;
+            $checksum = sha1($host, true);
+            $this->addTitle($host);
         }
         $notification = $this->requireCell()
             ->notifications()
             ->loadByChecksum($checksum);
 
-        $this->addSingleTab($this->translate('Notification'))
-            ->addTitle($this->translate('BEM Notification'));
+        $this->addSingleTab($this->translate('Notification'));
+        if ($notification) {
+            $details = new NameValueTable();
+            $details->addNameValueRow($this->translate('Host'), $host);
 
-        $this->content()
-            ->add(Html::pre(print_r($notification, 1)));
-        if (property_exists($notification, 'last_cmdline')) {
-            $this->content()->add(Html::pre($notification->last_cmdline));
+            if ($service !== null) {
+                $details->addNameValueRow($this->translate('Service'), $service);
+            }
+            $details->addNameValuePairs([
+                $this->translate('Last priority') => $notification->last_priority,
+                $this->translate('Last severity') => $notification->last_severity,
+                $this->translate('Notifications') => $notification->cnt_notifications,
+                $this->translate('First notification') => $notification->first_notification,
+                $this->translate('Last notification') => $notification->last_notification,
+                $this->translate('Next notification') => $notification->next_notification,
+            ]);
+
+            if ($notification->last_cmdline !== null) {
+                $details->addNameValueRow(
+                    $this->translate('Last command-line'),
+                    Html::pre($notification->last_cmdline)
+                );
+
+                $exitCode = $notification->last_exit_code;
+                if ($exitCode !== null) {
+                    $exitCode = (int) $exitCode;
+                    $details->addNameValuePairs([
+                        $this->translate('Last exit code') => $exitCode,
+                        $this->translate('Last output') => Html::pre($notification->last_output),
+                    ]);
+                }
+            }
+            $this->content()->add($details);
         } else {
-            die('adsf');
+            $this->content()->add('No issue found');
         }
     }
 }
