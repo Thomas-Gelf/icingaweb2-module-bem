ac2f7431648b285907455b052a14f3fcc05af5ff
Thomas Gelf <thomas@gelf.net>
2017-09-19 08:35:42 +0200

    schema/mysql: cleanup, naming

--- schema/mysql.sql
+++ schema/mysql.sql
@@ -1,6 +1,6 @@
-CREATE TABLE bmc_issue (
+CREATE TABLE bem_issue (
   checksum VARBINARY(20) NOT NULL COMMENT 'sha1(host), sha1(host!service)',
-  bmc_event_id BIGINT(20) UNSIGNED DEFAULT NULL,
+  bem_event_id BIGINT(20) UNSIGNED DEFAULT NULL,
   host VARCHAR(255) NOT NULL,
   service VARCHAR(255) DEFAULT NULL,
   last_priority ENUM(
@@ -8,7 +8,7 @@ CREATE TABLE bmc_issue (
     'PRIORITY_2',
     'PRIORITY_3',
     'PRIORITY_4',
-    'PRIORITY_5', -- lowest priority
+    'PRIORITY_5'-- lowest priority
   ) NOT NULL,
   last_severity ENUM(
         -- also seen:
@@ -19,13 +19,15 @@ CREATE TABLE bmc_issue (
         -- as of documentation:
         'MINOR',
         'MAJOR',
-        'CRITICAL',
+        'CRITICAL'
   ) NOT NULL,
   first_notification INT UNSIGNED NOT NULL,
   last_notification INT UNSIGNED NOT NULL,
   next_notification INT UNSIGNED NOT NULL,
   cnt_notifications INT UNSIGNED NOT NULL,
   last_exit_code TINYINT UNSIGNED NOT NULL,
-  last_cmd_line TEXT NOT NULL,
+  last_cmdline TEXT NOT NULL,
   last_output TEXT NOT NULL,
+  PRIMARY KEY (checksum),
+  INDEX idx_search (host (64), service(64))
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
