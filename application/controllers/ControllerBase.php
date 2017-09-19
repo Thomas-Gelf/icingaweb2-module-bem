d2beff039649392bdd898801b1d56b0b4e6b100e
Thomas Gelf <thomas@gelf.net>
2017-09-19 08:47:04 +0200

    ControllerBase: allow to require cell, get DB...
    ...from there

--- application/controllers/ControllerBase.php
+++ application/controllers/ControllerBase.php
@@ -2,12 +2,17 @@
 
 namespace Icinga\Module\Bem\Controllers;
 
+use Icinga\Module\Bem\Cell;
 use Icinga\Module\Bem\Config;
 use ipl\Web\CompatController;
 
 class ControllerBase extends CompatController
 {
+    /** @var \Zend_Db_Adapter_Abstract */
     private $db;
+
+    /** @var Cell */
+    private $cell;
 
     /**
      * @return \Zend_Db_Adapter_Abstract
@@ -15,6 +20,15 @@ class ControllerBase extends CompatController
     protected function db()
     {
         if ($this->db === null) {
+            $this->db = $this->requireCell()->getIdo()->getDb();
+        }
+
+        return $this->db;
+    }
+
+    protected function requireCell()
+    {
+        if ($this->cell === null) {
             $config = new Config();
             $cell = $this->params->get('cell');
             if ($cell === null) {
@@ -22,9 +36,9 @@ class ControllerBase extends CompatController
                 $this->redirectNow($this->url()->with('cell', $cell));
             }
 
-            $this->db = $config->getCell($this->params->getRequired('cell'))->getIdo();
+            $this->cell = $config->getCell($cell);
         }
 
-        return $this->db;
+        return $this->cell;
     }
 }
