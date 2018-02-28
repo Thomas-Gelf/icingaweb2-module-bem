6b1030fed89acba62413355b5d387c0e07c4cc0f
Thomas Gelf <thomas@gelf.net>
2018-02-28 17:44:54 +0100

    Command: add new base CLI command

---
+++ application/clicommands/Command.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Icinga\Module\Bem\Clicommands;
+
+use Icinga\Cli\Command as CliCommand;
+use Icinga\Module\Bem\IdoDb;
+use Icinga\Module\Bem\MainRunner;
+
+class Command extends CliCommand
+{
+    public function init()
+    {
+        // We need ipl3rdparty, monitoring
+        $this->app->getModuleManager()->loadEnabledModules();
+    }
+
+    /**
+     * @return MainRunner
+     */
+    protected function getRunner()
+    {
+        return new MainRunner($this->params->getRequired('cell'));
+    }
+
+    /**
+     * @return IdoDb
+     */
+    protected function getIdo()
+    {
+        return IdoDb::fromMonitoringModule();
+    }
+}
--- application/clicommands/EventCommand.php
+++ application/clicommands/EventCommand.php
@@ -2,17 +2,11 @@
 
 namespace Icinga\Module\Bem\Clicommands;
 
-use Icinga\Cli\Command;
-use Icinga\Module\Bem\IdoDb;
-use Icinga\Module\Bem\MainRunner;
+use Icinga\Module\Bem\BemNotification;
+use Icinga\Module\Bem\Config\CellConfig;
 
 class EventCommand extends Command
 {
-    public function init()
-    {
-        // We need ipl3rdparty, monitoring
-        $this->app->getModuleManager()->loadEnabledModules();
-    }
 
     public function senderAction()
     {
@@ -21,20 +15,10 @@ class EventCommand extends Command
 
     public function sendAction()
     {
-        $ido = IdoDb::fromMonitoringModule();
-
-        $object = $ido->fetchObject(
+        $object = $this->getIdo()->fetchObject(
             $this->params->getRequired('host'),
             $this->params->get('service')
         );
         $this->getRunner()->runOnceFor($object);
-    }
-
-    /**
-     * @return MainRunner
-     */
-    protected function getRunner()
-    {
-        return new MainRunner($this->params->getRequired('cell'));
     }
 }
