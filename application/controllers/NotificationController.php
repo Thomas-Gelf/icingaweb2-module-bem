fa7badec721a0e115711f2aee35dbf52776fd4ac
Thomas Gelf <thomas@gelf.net>
2018-03-01 14:47:32 +0100

    NotificationController: use BemNotification

--- application/controllers/NotificationController.php
+++ application/controllers/NotificationController.php
@@ -2,29 +2,21 @@
 
 namespace Icinga\Module\Bem\Controllers;
 
+use Icinga\Module\Bem\BemNotification;
 use Icinga\Module\Bem\Web\Widget\NotificationDetails;
 
 class NotificationController extends ControllerBase
 {
     public function indexAction()
     {
-        if ($service = $this->params->get('service')) {
-            $host = $this->params->get('host');
-            $checksum = sha1($host . $service, true);
-            $this->addTitle('%s: %s', $host, $service);
-        } else {
-            $host = $this->params->get('host');
-            $service = null;
-            $checksum = sha1($host, true);
-            $this->addTitle($host);
-        }
-        $notification = $this->requireCell()
-            ->notifications()
-            ->loadByChecksum($checksum);
+        $notification = BemNotification::loadFromLog(
+            $this->requireCell(),
+            $this->params->getRequired('id')
+        );
 
         $this->addSingleTab($this->translate('Notification'));
         if ($notification) {
-            $details = new NotificationDetails($notification, $host, $service);
+            $details = new NotificationDetails($notification);
             $this->content()->add($details);
         } else {
             $this->content()->add('No issue found');
