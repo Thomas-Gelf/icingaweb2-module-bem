3acf8bb2b7d31feba8cb0e850653d623dfd5f3ea
Thomas Gelf <thomas@gelf.net>
2018-03-01 13:46:28 +0100

    IssuesController: new controller, not yet ready

---
+++ application/controllers/IssuesController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Icinga\Module\Bem\Controllers;
+
+use dipl\Html\Table;
+use Icinga\Module\Bem\BemIssueTable;
+
+class IssuesController extends ControllerBase
+{
+    public function init()
+    {
+        $this->prepareTabs();
+    }
+
+    public function indexAction()
+    {
+        $this->addTitle($this->translate('Current Issues'));
+
+        $table = new Table();
+        (BemIssueTable::forCell($this->requireCell()))->renderTo($this);
+        $this->content()->add($table);
+    }
+}
