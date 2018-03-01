d2a7e51c7224fe4207a63e4fe42bcfdb9aa7957a
Thomas Gelf <thomas@gelf.net>
2018-03-01 15:25:50 +0100

    NotificationsController: cleanup

--- application/controllers/NotificationsController.php
+++ application/controllers/NotificationsController.php
@@ -2,8 +2,6 @@
 
 namespace Icinga\Module\Bem\Controllers;
 
-use dipl\Html\Table;
-use Icinga\Module\Bem\BemIssueTable;
 use Icinga\Module\Bem\Web\Table\NotificationLogTable;
 
 class NotificationsController extends ControllerBase
@@ -16,9 +14,6 @@ class NotificationsController extends ControllerBase
     public function indexAction()
     {
         $this->addTitle($this->translate('Notifications that have been sent'));
-
-        $table = new Table();
-        (NotificationLogTable::forCell($this->requireCell()))->renderTo($this);
-        $this->content()->add($table);
+        NotificationLogTable::forCell($this->requireCell())->renderTo($this);
     }
 }
