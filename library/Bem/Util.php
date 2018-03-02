baee8160be9e00087865e050e9f7cd78e0058979
Thomas Gelf <thomas@gelf.net>
2018-03-02 10:15:57 +0100

    Util: css class for severity helper

--- library/Bem/Util.php
+++ library/Bem/Util.php
@@ -4,6 +4,17 @@ namespace Icinga\Module\Bem;
 
 class Util
 {
+    protected static $severityCssClassMap = [
+        'UNKNOWN'  => 'state-unknown',
+        'OK'       => 'state-ok',
+        'INFO'     => 'state-ok',
+        'WARNING'  => 'state-warning',
+        'MINOR'    => 'state-warning',
+        'MAJOR'    => 'state-critical',
+        'CRITICAL' => 'state-critical',
+        'DOWN'     => 'state-down'
+    ];
+
     /**
      * @return int
      */
@@ -12,5 +23,14 @@ class Util
         $mTime = explode(' ', microtime());
 
         return (int) round($mTime[0] * 1000) + (int) $mTime[1] * 1000;
+    }
+
+    public static function cssClassForSeverity($severity)
+    {
+        if (array_key_exists($severity, static::$severityCssClassMap)) {
+            return static::$severityCssClassMap[$severity];
+        } else {
+            return 'state-pending';
+        }
     }
 }
