2df15f24e4ef70865ffa65a6065a0f6d9ccfc234
Thomas Gelf <thomas@gelf.net>
2018-03-02 10:15:14 +0100

    NotificationCommand: introduce scheduleCommand

--- application/clicommands/NotificationCommand.php
+++ application/clicommands/NotificationCommand.php
@@ -2,20 +2,70 @@
 
 namespace Icinga\Module\Bem\Clicommands;
 
+use Icinga\Module\Bem\BemIssue;
 use Icinga\Module\Bem\BemNotification;
 use Icinga\Module\Bem\Config\CellConfig;
 
 class NotificationCommand extends Command
 {
     /**
-     * icingacli bem notification send --cell <cell_name> [options]
+     * USAGE
+     * -----
+     * icingacli bem notification schedule --cell <cell_name> [options]
      *
-     * --host_name <host>
-     * --service_name <service>
-     * --host.vars.<varname> <varvalue>
-     * --service.vars.<varname> <varvalue>
-     * --state <state>
-     * --output <output>
+     * OPTIONS
+     * -------
+     *   --host_name <host>
+     *   --service_name <service>
+     *   --host.vars.<varname> <varvalue>
+     *   --service.vars.<varname> <varvalue>
+     *   --state <state>
+     *   --output <output>
+     */
+    public function scheduleAction()
+    {
+        $p = $this->params;
+        $config = CellConfig::loadByName($p->shiftRequired('cell'));
+        $p->getRequired('host_name');
+        $p->getRequired('state');
+        $p->getRequired('output');
+
+        $object = $this->params->getParams();
+        $object['state_type'] = 'HARD';
+        if (array_key_exists('service_name', $object)) {
+            $object['object_type'] = 'service';
+        } else {
+            $object['object_type'] = 'host';
+            $object['service_name'] = null;
+        }
+        $object = (object) $object;
+        if ($config->wants($object)) {
+            $issue = BemIssue::forIcingaObject($object, $config);
+            $isNew = $issue->isNew();
+            $issue->scheduleNextNotification()->store();
+            if ($isNew) {
+                echo "Scheduled a new issue\n";
+            } else {
+                echo "Rescheduled an existing issue\n";
+            }
+        } else {
+            echo "This is not a problem, not scheduled\n";
+        }
+    }
+
+    /**
+     * USAGE
+     * -----
+     * icingacli bem notification schedule --cell <cell_name> [options]
+     *
+     * OPTIONS
+     * -------
+     *   --host_name <host>
+     *   --service_name <service>
+     *   --host.vars.<varname> <varvalue>
+     *   --service.vars.<varname> <varvalue>
+     *   --state <state>
+     *   --output <output>
      */
     public function sendAction()
     {
@@ -35,11 +85,9 @@ class NotificationCommand extends Command
         }
 
         $notification = BemNotification::forIcingaObject((object) $object, $config);
-
         if ($config->wants((object) $object)) {
             $poster = $config->getImpactPoster();
-            echo "DEBUG: " . $poster->getCommandString($notification) . "\n";
-            $poster->send($notification);
+            echo $poster->getCommandString($notification) . "\n";
         } else {
             echo "This is not a problem, might be cleared\n";
         }
