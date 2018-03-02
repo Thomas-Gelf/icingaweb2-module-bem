dcde38aedd575791362f92d85823b42064cf917b
Thomas Gelf <thomas@gelf.net>
2018-03-02 12:06:13 +0100

    configuration: add to main menu

--- configuration.php
+++ configuration.php
@@ -1,5 +1,8 @@
 <?php
 
 /** @var Icinga\Application\Modules\Module $this */
-$section = $this->menuSection(N_('Problems'));
-$section->add(N_('BEM Notifications'))->setUrl('bem');
+$section = $this->menuSection(N_('BEM Notifications'))
+    ->setIcon('bell-alt');
+$section->add(N_('Current Issues'))->setUrl('bem/issues');
+$section->add(N_('Sent Notifications'))->setUrl('bem/notifications');
+$section->add(N_('Configured Cells'))->setUrl('bem');
